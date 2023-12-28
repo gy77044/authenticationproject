ed3a544cd7871a73f8dce279f884b54001d58c74
gautam yadav <gy77044@gmail.com>
2023-12-28 23:49:39 +0530

    added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
 export default function App() {
-  return <div className='text-red-500'>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
